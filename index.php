2044893257ea92d51b9c279f0a5cabe3bf14e049
Jipay <dcl.jeanphig@18pixel.fr>
2018-11-08 14:43:26 +0100

    css en cour

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+    <title>Tables Multiplication</title>
+</head>
+<body>
+    <header>
+            <h2>Tables de Multiplication</h2>
+    </header>
+    <main>
+        <div class="container1">
+            <form action="#" class="choixTables" id="choixTables" method="GET">
+                <select name="tables">
+                    <option value="table1">Table de 1</option>
+                    <option value="table2">Table de 2</option>
+                    <option value="table3">Table de 3</option>
+                    <option value="table4">Table de 4</option>
+                    <option value="table5">Table de 5</option>
+                    <option value="table6">Table de 6</option>
+                    <option value="table7">Table de 7</option>
+                    <option value="table8">Table de 8</option>
+                    <option value="table9">Table de 9</option>
+                    <option value="table10">Table de 10</option>
+                </select>
+                <p class="choixText">Choix de la table à afficher</p>
+                <input type="submit" class="voir" value="voir">
+            </form>
+        </div>
+    </main>
+    <footer>
+    </footer>
+    <script src="script.js"></script>
+</body>
+</html>
