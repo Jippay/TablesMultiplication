b6678ad707a50c8ccf09e105b2aa7357234183a5
Jipay <dcl.jeanphig@18pixel.fr>
2018-11-09 13:23:02 +0100

    vendredi 13h30

--- cible.php
+++ cible.php
@@ -1,11 +1,13 @@
 <?php
+
 $table = $_GET["tables"];
 $total = 'Table de multiplication de '.$table.'<br />';
 $x=0;
 
 for ($x = 0; $x <= 30; $x++) {
     $total .= $x." X ".$table." = ".$x*$table."<br />";
-    
-} 
+        
+    }
+
 echo $total;
 ?>
--- index.php
+++ index.php
@@ -1,3 +1,5 @@
+
+
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -8,12 +10,13 @@
     <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
     <title>Tables Multiplication</title>
 </head>
+
 <body>
     <header>
             <h2>Tables de Multiplication</h2>
     </header>
     <main>
-        <form class="choixTables" id="choixTables" method="GET" action="cible.php">
+        <form class="choixTables" id="choixTables" method="GET" action="index.php">
             <select id="tables" name="tables">
                 <option value="1">Table de 1</option>
                 <option value="2">Table de 2</option>
@@ -29,7 +32,20 @@
             <p class="choixText">Choix de la table à afficher</p>
             <input type="submit" id="voir" class="voir" value="voir">
         </form>
-        <?php include 'cible.php';?>
+        
+    <?php
+    if ( isset($_GET['tables'])) {
+        $table = $_GET["tables"];
+        $total = 'Table de multiplication de '.$table.'<br />';
+        $x=0;
+            for ($x = 0; $x <= 30; $x++) {
+                $total .= $x." X ".$table." = ".$x*$table."<br />";
+            }
+        echo $total;
+        
+    }
+    ?>
+    
     </main>
     <footer>
     </footer>
